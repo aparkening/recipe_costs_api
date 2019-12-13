54d666e7b8beca8c0ade7b1780981c22ae5dd56c
aparkening <aaron.parkening@gmail.com>
2019-12-13 17:07:12 -0500

    Add relationships and validations.

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,8 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  # Validations
+  validates :ingredient_amount, presence: true, numericality: true
+  validates :ingredient_unit, presence: true  
 end
