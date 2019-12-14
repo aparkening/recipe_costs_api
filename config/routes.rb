d19955513381a8e51d88aaa3caab2114ccddc5aa
aparkening <aaron.parkening@gmail.com>
2019-12-13 20:44:09 -0500

    Add ingredient and recipe routes to api/v1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  resources :recipe_ingredients
-  resources :ingredients
-  resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Root
+  root 'application#index'
+
+  namespace :api do
+    namespace :v1 do
+      # Ingredients
+      resources :ingredients
+      post 'ingredients/import'
+
+      # Recipes
+      resources :recipes
+      get 'recipes/ingredients/:id' => 'recipes#by_ingredient', as: "recipes_by_ingredient"
+      post 'recipes/import'
+    end
+  end
+
+
 end
