4e1117dff0dd2e16de112d8cbe450453faa3e810
aparkening <aaron.parkening@gmail.com>
2019-12-17 14:59:22 -0500

    Schema includes new recipes attributes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_220002) do
+ActiveRecord::Schema.define(version: 2019_12_17_193141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2019_12_13_220002) do
     t.integer "servings"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "total_cost"
+    t.float "cost_per_serving"
   end
 
   add_foreign_key "recipe_ingredients", "ingredients"
