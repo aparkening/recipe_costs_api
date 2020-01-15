a250f4ca1c5982c68e2c4d264082f556bf6f5cc5
aparkening <aaron.parkening@gmail.com>
2020-01-15 16:11:13 -0500

    Remove old commented out code.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -54,37 +54,14 @@ class Recipe < ApplicationRecord
     return recipe_names
   end
 
+  # Calculate total recipe cost
+  def calc_cost(ingredient_costs)
+    ingredient_costs.inject(0){|sum,x| sum + x.total_cost }.round(2)
+  end
 
- ## Instance versions 
-    # Calculate recipe cost
-    # def ingredient_costs
-    #   recipe_total = 0
-    #   self.recipe_ingredients.map do |ingredient|
-    #     # Add cost to ingredient
-    #     combo_ingredient = CombinedIngredient.new(ingredient).total_cost
-    #     # recipe_total += combo_ingredient.total_cost
-    #   end
-    # end
-
-    # Calculate total recipe cost
-    def calc_cost(ingredient_costs)
-      ingredient_costs.inject(0){|sum,x| sum + x.total_cost }.round(2)
-    end
-
-    # Calculate recipe cost per serving
-    def calc_cost_per_serving(total_cost)
-      (total_cost/self.servings).round(2)
-    end
-
-
-  # Writer for custom accepts_nested_attributes_for
-  # def recipe_ingredients=(ingredients_attributes)
-
-  #   ingredient = Ingredient.find(ingredients_attributes[:ingredient_id])
-
-  #   if !self.recipe_ingredients.include?(ingredient)
-  #     self.recipe_ingredients.build(:ingredient => ingredient, :ingredient_amount => ingredients_attributes[:ingredient_amount], :ingredient_unit => ingredients_attributes[:ingredient_unit])
-  #   end 
-  # end
+  # Calculate recipe cost per serving
+  def calc_cost_per_serving(total_cost)
+    (total_cost/self.servings).round(2)
+  end
 
 end
