f7934f9792e7241f2d5a7e294d43616cc2ec5801
aparkening <aaron.parkening@gmail.com>
2020-04-30 15:49:32 -0400

    Add recipe ingredients for cinnamon raisin bread, pizza dough, and french bread.

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,19 +46,50 @@ salt = Ingredient.create(name: "salt", cost: 18.13, cost_size: 25, cost_unit: "l
 choc_chips = Ingredient.create(name: "chocolate chips", cost: 15.35, cost_size: 5, cost_unit: "lb")
 b_soda = Ingredient.create(name: "baking soda", cost: 24.84, cost_size:50, cost_unit: "lb")
 walnuts = Ingredient.create(name: "walnuts", cost: 98.75, cost_size:25, cost_unit: "lb")
+instant_yeast = Ingredient.create(name: "instant yeast", cost: 48.99, cost_size: 20, cost_unit: "lb")
+ground_cinnamon = Ingredient.create(name: "ground cinnamon", cost: 77.50, cost_size: 25, cost_unit: "lb")
+milk = Ingredient.create(name: "milk", cost: 3.99, cost_size: 8, cost_unit: "lb")
+raisins = Ingredient.create(name: "raisins", cost: 55.20, cost_size: 30, cost_unit: "lb")
+olive_oil = Ingredient.create(name: "olive oil", cost: 16.49, cost_size: 7.6, cost_unit: "lb")
 
 
-# Add ingredients to recipes
+# Recipe Ingredients
 # -> Upload full set from recipe CSVs in app/assets
 
 #  Chocolate Chip Cookies recipe ingredients
-cc_butter = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("butter"), ingredient_amount:2, ingredient_unit:"lb")
-cc_sugar = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("sugar"), ingredient_amount:35, ingredient_unit:"oz")
-cc_molasses = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("molasses"), ingredient_amount:1.42, ingredient_unit:"oz")
-cc_v_extract = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("vanilla extract"), ingredient_amount:0.5, ingredient_unit:"oz")
-cc_v_egg = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("egg"), ingredient_amount:8, ingredient_unit:"each")
-cc_flour = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("all-purpose flour"), ingredient_amount:2.5, ingredient_unit:"lb")
-cc_b_soda = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("baking soda"), ingredient_amount:0.9, ingredient_unit:"oz")
-cc_salt = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("salt"), ingredient_amount:0.75, ingredient_unit:"oz")
-cc_choc_chips = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("chocolate chips"), ingredient_amount:36, ingredient_unit:"oz")
-cc_walnuts = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("walnuts"), ingredient_amount:8, ingredient_unit:"oz")
+cc_butter = choc_chip_cookies.recipe_ingredients.create(ingredient: butter, ingredient_amount:2, ingredient_unit:"lb")
+cc_sugar = choc_chip_cookies.recipe_ingredients.create(ingredient: sugar, ingredient_amount:35, ingredient_unit:"oz")
+cc_molasses = choc_chip_cookies.recipe_ingredients.create(ingredient: molasses, ingredient_amount:1.42, ingredient_unit:"oz")
+cc_v_extract = choc_chip_cookies.recipe_ingredients.create(ingredient: v_extract, ingredient_amount:0.5, ingredient_unit:"oz")
+cc_v_egg = choc_chip_cookies.recipe_ingredients.create(ingredient: egg, ingredient_amount:8, ingredient_unit:"each")
+cc_flour = choc_chip_cookies.recipe_ingredients.create(ingredient: flour, ingredient_amount:2.5, ingredient_unit:"lb")
+cc_b_soda = choc_chip_cookies.recipe_ingredients.create(ingredient: b_soda, ingredient_amount:0.9, ingredient_unit:"oz")
+cc_salt = choc_chip_cookies.recipe_ingredients.create(ingredient: salt, ingredient_amount:0.75, ingredient_unit:"oz")
+cc_choc_chips = choc_chip_cookies.recipe_ingredients.create(ingredient: choc_chips, ingredient_amount:36, ingredient_unit:"oz")
+cc_walnuts = choc_chip_cookies.recipe_ingredients.create(ingredient: walnuts, ingredient_amount:8, ingredient_unit:"oz")
+
+# Pattern to create ingredient with unknown ingredient variable
+# cc_walnuts = choc_chip_cookies.recipe_ingredients.create(ingredient:Ingredient.find_by_name("walnuts"), ingredient_amount:8, ingredient_unit:"oz")
+
+# Cinnamon Raisin Bread recipe ingredients
+cinn_raisin_bread.recipe_ingredients.create(ingredient: b_flour, ingredient_amount:16, ingredient_unit:"oz")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: sugar, ingredient_amount:0.66, ingredient_unit:"oz")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: salt, ingredient_amount:0.31, ingredient_unit:"oz")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: instant_yeast, ingredient_amount:0.22, ingredient_unit:"oz")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: ground_cinnamon, ingredient_amount:0.16, ingredient_unit:"oz")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: egg, ingredient_amount:1, ingredient_unit:"each")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: butter, ingredient_amount:1, ingredient_unit:"oz")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: milk, ingredient_amount:4, ingredient_unit:"oz")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: raisins, ingredient_amount:9, ingredient_unit:"oz")
+cinn_raisin_bread.recipe_ingredients.create(ingredient: walnuts, ingredient_amount:4, ingredient_unit:"oz")
+
+# Pizza Dough recipe ingredients
+pizza_dough.recipe_ingredients.create(ingredient: b_flour, ingredient_amount:20.25, ingredient_unit:"oz")
+pizza_dough.recipe_ingredients.create(ingredient: salt, ingredient_amount:0.44, ingredient_unit:"oz")
+pizza_dough.recipe_ingredients.create(ingredient: instant_yeast, ingredient_amount:0.11, ingredient_unit:"oz")
+pizza_dough.recipe_ingredients.create(ingredient: olive_oil, ingredient_amount:2, ingredient_unit:"oz")
+
+# French Bread recipe ingredients
+french_bread.recipe_ingredients.create(ingredient: b_flour, ingredient_amount:27, ingredient_unit:"oz")
+french_bread.recipe_ingredients.create(ingredient: salt, ingredient_amount:0.56, ingredient_unit:"oz")
+french_bread.recipe_ingredients.create(ingredient: instant_yeast, ingredient_amount:0.19, ingredient_unit:"oz")
